905246a1b2b2d6905d75017608dddce7c89ad49c
doraxhe <doraapplication@gmail.com>
2020-10-12 23:40:37 -0400

    Finished.

--- src/main/java/cd/get/ready/algorithms/ArrayRotator.java
+++ src/main/java/cd/get/ready/algorithms/ArrayRotator.java
@@ -4,8 +4,17 @@ public class ArrayRotator {
 
     public int[] rotateLeft(int[] arr, int n){
 
-        return arr;
+        int mod = n % arr.length;
+        int[] result = new int[arr.length];
+
+        for (int i = 0; i < result.length; i++) {
+            if (i + mod <= result.length - 1) {
+                result[i] = arr[i + mod];
+            } else {
+                result[i] = arr[i + mod - arr.length];
+            }
+        }
+
+        return result;
     }
-
-
 }
